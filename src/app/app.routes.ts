3f2b79cf895f19144498f57a2733686acacaff8d
Alejoski <napardoz@gmail.com>
2024-09-26 19:15:32 -0500

    Se implementa el menu y algunas acciones, adicionalmente cascaron de home y caracteristicas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { TeamCharacteristicsComponent } from './components/team-characteristics/team-characteristics.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'team-char', component: TeamCharacteristicsComponent },
+  //{ path: 'team-char', component: TeamCharacteristicsComponent },
+  { path: '**', redirectTo:'', pathMatch:'full'}
+
+];
---
+++ src/app/components/team-characteristics/team-characteristics.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-team-characteristics',
+  standalone: true,
+  imports: [],
+  templateUrl: './team-characteristics.component.html',
+  styleUrl: './team-characteristics.component.css'
+})
+export class TeamCharacteristicsComponent {
+
+}
